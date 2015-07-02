e546dc92a7c0377ad91a721b8a98543e3add10c3
chinakids <494063570@qq.com>
2015-07-02 08:37:17 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	require('time-grunt')(grunt);
+
+  grunt.initConfig({
+	  watch: {
+		  less: {
+		    files: [
+		        'public/less/**/*.less'
+		    ],
+		    tasks: ['less']
+		  }
+		},
+		less: {
+      options: {
+        compress: true,
+        yuicompress: false,
+        sourceMap:true
+      },
+      style:{
+        files: [{
+          expand: true,
+          cwd: 'public/less',
+          src: ['**/*.less'],
+          dest: 'public/stylesheets',
+          ext: '.min.css'
+        }]
+      }
+    }
+  })
+    // 注冊任务
+    grunt.registerTask('default', [
+    	'less',
+      'watch'
+    	]);
+    grunt.registerTask('test', ['jshint']);
+    grunt.event.on('watch', function(action, filepath, target) {
+	  grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+	});
+};
