2e019c59f7c563402193d4cb6cdd90365082abca
chinakids <494063570@qq.com>
2015-07-09 08:37:17 +0800

    细节调整...

--- routes/index.js
+++ routes/index.js
@@ -19,26 +19,26 @@ var $ = Zepto(window);
 //初始化 marked 参数
 var renderer = new markdown.Renderer();
 renderer.listitem = function(text) {
-  //console.log(text);
-  //console.log(!/^\[[ x]\]\s/.test(text))
-  //console.log($('<input type="checkbox" disabled/>'));
-  console.log('/////////////listitem')
+  ////console.log(text);
+  ////console.log(!/^\[[ x]\]\s/.test(text))
+  ////console.log($('<input type="checkbox" disabled/>'));
+  //console.log('/////////////listitem')
   if (!/^\[[ x]\]\s/.test(text)) {
     return markdown.Renderer.prototype.listitem(text);
   }
   // 任务列表
   var checkbox = $('<input type="checkbox" disabled/>');
-  //console.log(checkbox[0])
+  ////console.log(checkbox[0])
   if (/^\[x\]\s/.test(text)) { // 完成的任务列表
     checkbox.attr('checked', true);
   }
-  //console.log($(markdown.Renderer.prototype.listitem(text.substring(3))).addClass(
+  ////console.log($(markdown.Renderer.prototype.listitem(text.substring(3))).addClass(
   //'task-list-item').prepend(checkbox)[0].outerHTML)
   return $(markdown.Renderer.prototype.listitem(text.substring(3))).addClass(
     'task-list-item').prepend(checkbox)[0].outerHTML;
 }
 renderer.codespan = function(text) { // inline code
-  console.log('/////////////codespan')
+  //console.log('/////////////codespan')
   if (/^\$.+\$$/.test(text)) { // inline math
     var raw = /^\$(.+)\$$/.exec(text)[1];
     var line = raw.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(
@@ -54,11 +54,11 @@ renderer.codespan = function(text) { // inline code
   return markdown.Renderer.prototype.codespan.apply(this, arguments);
 }
 renderer.code = function(code, language, escaped, line_number) {
-  console.log('/////////////code')
+  //console.log('/////////////code')
   code = code.trim();
   var firstLine = code.split(/\n/)[0].trim();
   if (language === 'math') { // 数学公式
-    console.log('/////////////code/////math')
+    //console.log('/////////////code/////math')
     var tex = '';
     code.split(/\n\n/).forEach(function(line) { // 连续两个换行，则开始下一个公式
       line = line.trim();
@@ -75,13 +75,13 @@ renderer.code = function(code, language, escaped, line_number) {
     return '<div data-line="' + line_number + '">' + tex + '</div>';
   } else if (firstLine === 'gantt' || firstLine === 'sequenceDiagram' ||
     firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) { // mermaid
-    console.log('/////////////code/////mermaid')
-    console.log(firstLine);
+    //console.log('/////////////code/////mermaid')
+    //console.log(firstLine);
     if (firstLine === 'sequenceDiagram') {
       code += '\n'; // 如果末尾没有空行，则语法错误
     }
 
-    //console.log(mermaid.mermaidAPI.parse(code))
+    ////console.log(mermaid.mermaidAPI.parse(code))
     if (mermaid.mermaidAPI.parse(code)) {
       return '<div class="mermaid" data-line="' + line_number + '">' + code +
         '</div>';
@@ -90,18 +90,18 @@ renderer.code = function(code, language, escaped, line_number) {
         '</pre>';
     }
   } else {
-    console.log('/////////////code/////other')
+    //console.log('/////////////code/////other')
     return markdown.Renderer.prototype.code.apply(this, arguments);
   }
 };
 renderer.html = function(html) {
-  console.log('/////////////html')
+  //console.log('/////////////html')
   var result = markdown.Renderer.prototype.html.apply(this, arguments);
   var h = $(result.bold());
   return h.html();
 };
 renderer.paragraph = function(text) {
-  console.log('/////////////paragraph')
+  //console.log('/////////////paragraph')
   var result = markdown.Renderer.prototype.paragraph.apply(this, arguments);
   var h = $(result.bold());
   // h.find('script,iframe').remove();
@@ -146,10 +146,10 @@ router.get('/', function(req, res, next) {
   var buffStr = fs.readFileSync(path.join(__dirname, '../doc/readme.md'),
     'utf8');
   var html = markdown(buffStr);
-  console.log(html);
+  //console.log(html);
   mdtools.getMenu(html, function(html, menu) {
     res.render('index', {
-      title: '首页',
+      title: 'Wiki 文档中心',
       html: html,
       menu: menu,
       tree: treeNode,
@@ -162,11 +162,11 @@ router.get('/book', function(req, res, next) {
   var md = req.query.md;
   var buffStr = fs.readFileSync(path.join(__dirname, '../doc/' + md + '.md'),
     'utf8');
-  console.log(buffStr)
+  //console.log(buffStr)
   var html = markdown(buffStr);
-  console.log(html);
+  //console.log(html);
   mdtools.getMenu(html, function(html, menu) {
-    console.log(treeNode);
+    //console.log(treeNode);
     //res.send('ok')
     res.render('index', {
       title: md.split('/')[md.split('/').length - 1],
